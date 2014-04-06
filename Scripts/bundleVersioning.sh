384ddf53a7d25e3c7e38d82edf2bb4494668f935
Konstantin Koval <konstantin@rocketfarm.no>
2014-04-06 17:16:08 +0200

    improve build number counting

--- Scripts/bundleVersioning.sh
+++ Scripts/bundleVersioning.sh
@@ -1,3 +1,3 @@
 #Update build number with number of git commits
-buildNumber=$(git rev-list HEAD | wc -l | tr -d ' ')
+buildNumber=`git rev-list HEAD --count`
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${PROJECT_DIR}/${INFOPLIST_FILE}"
